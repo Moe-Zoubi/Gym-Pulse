87de55bf738a2207e883904a97211f7f65e4eaf8
unknown <m.zoubi2@hotmail.com>
2024-01-11 17:51:11 +0300

    added db for classes and schedule

---
+++ src/data/ClassData.js
@@ -0,0 +1,67 @@
+
+
+
+const ClassData = [
+    {
+        classname: "Cycling",
+        trainer: "Dorian Yate",
+        date: "Wed: 9:00 am",
+    },
+    {
+        classname: "Meditation",
+        trainer: "Maria Mich",
+        date: "Fri: 1:00 pm",
+    },
+    {
+        classname: "Boxing",
+        trainer: "John Flex",
+        date: "Tue: 4:00 pm",
+    },
+    {
+        classname: "Karate",
+        trainer: "David Rich",
+        date: "Sat: 9:00 am",
+    },
+    {
+        classname: "Power Lifting",
+        trainer: "Larry Wheels",
+        date: "Mon: 8:00 pm",
+    },
+    {
+        classname: "Workout",
+        trainer: "Shawn Ray",
+        date: "Tue: 10:00 am",
+    },
+    {
+        classname: "Crossfit",
+        trainer: "Jenifer Alex",
+        date: "Wen: 9:00 pm",
+    },
+    {
+        classname: "Running",
+        trainer: "Zinia Zessy",
+        date: "Thu: 12:00 am",
+    },
+    {
+        classname: "Fitness",
+        trainer: "Mia Paul",
+        date: "Fri: 12:00 pm",
+    },
+    {
+        classname: "Body Building",
+        trainer: "Paul Sneed",
+        date: "Mon: 8:00 pm",
+    },
+    {
+        classname: "Mma",
+        trainer: "Becky Lynch",
+        date: "Fri: 6:00 am",
+    },
+    {
+        classname: "Yoga",
+        trainer: "Marta Mich",
+        date: "Wen: 8:00 am",
+    },
+]
+
+export default ClassData;
---
+++ src/data/ScheduleData.js
@@ -0,0 +1,133 @@
+
+
+
+const ScheduleData = [
+    {
+        classname: "Fitness",
+        time: "9:00am - 10:00am",
+        trainer: "David Vila",
+        day: "Monday",
+    },
+    {
+        classname: "Body Building",
+        time: "10:00pm - 11:00pm",
+        trainer: "John Weights",
+        day: "Monday",
+    },
+    {
+        classname: "Running",
+        time: "4:00pm - 5:00pm",
+        trainer: "Junifor Jonas",
+        day: "Monday",
+    },
+    {
+        classname: "Cycling",
+        time: "6:00pm - 7:00pm",
+        trainer: "Robert Louis",
+        day: "Monday",
+    },
+    {
+        classname: "Boxing",
+        time: "9:00am - 10:00am",
+        trainer: "Mike Tyson",
+        day: "Tuesday",
+    },
+    {
+        classname: "Yoga",
+        time: "10:00pm - 11:00pm",
+        trainer: "Celina Turner",
+        day: "Tuesday",
+    },
+    {
+        classname: "Crossfit",
+        time: "4:00pm - 5:00pm",
+        trainer: "Cristofer Columbo",
+        day: "Tuesday",
+    },
+    {
+        classname: "Cycling",
+        time: "9:00am - 10:00am",
+        trainer: "Mario Vreco",
+        day: "Wednesday",
+    },
+    {
+        classname: "Body Building",
+        time: "10:00pm - 11:00pm",
+        trainer: "John Weights",
+        day: "Wednesday",
+    },
+    {
+        classname: "Fitness",
+        time: "4:00pm - 5:00pm",
+        trainer: "Tom Rizzly",
+        day: "Wednesday",
+    },
+    {
+        classname: "Running",
+        time: "6:00pm - 7:00pm",
+        trainer: "Michael Jonson",
+        day: "Wednesday",
+    },
+    {
+        classname: "Fitness",
+        time: "9:00am - 10:00am",
+        trainer: "David Vila",
+        day: "Thursday",
+    },
+    {
+        classname: "Body Building",
+        time: "10:00pm - 11:00pm",
+        trainer: "John Weights",
+        day: "Thursday",
+    },
+    {
+        classname: "Running",
+        time: "4:00pm - 5:00pm",
+        trainer: "Junifor Jonas",
+        day: "Thursday",
+    },
+    {
+        classname: "Crossfit",
+        time: "9:00am - 10:00am",
+        trainer: "Mike Mich",
+        day: "Friday",
+    },
+    {
+        classname: "Karate",
+        time: "10:00pm - 11:00pm",
+        trainer: "John Jones",
+        day: "Friday",
+    },
+    {
+        classname: "Meditation",
+        time: "4:00pm - 5:00pm",
+        trainer: "Lara Croft",
+        day: "Friday",
+    },
+    {
+        classname: "Workout",
+        time: "6:00pm - 7:00pm",
+        trainer: "Lary Wheels",
+        day: "Friday",
+    },
+    {
+        classname: "Power Lifting",
+        time: "9:00am - 10:00am",
+        trainer: "David Laid",
+        day: "Saturday",
+    },
+    {
+        classname: "Boxing",
+        time: "8:00pm - 9:00pm",
+        trainer: "Tyson Fury",
+        day: "Saturday",
+    },
+    {
+        classname: "Yoga",
+        time: "4:00pm - 5:00pm",
+        trainer: "Milica Tails",
+        day: "Saturday",
+    },
+]
+
+export default ScheduleData;

1aacacca2157aead0c679203ede651db9eebdddd
unknown <m.zoubi2@hotmail.com>
2024-01-13 06:30:04 +0300

    added classes database

--- src/data/ClassData.js
+++ src/data/ClassData.js
@@ -6,61 +6,73 @@ const ClassData = [
         classname: "Cycling",
         trainer: "Dorian Yate",
         date: "Wed: 9:00 am",
+        img:"../images/classes/cycling.jpg",
     },
     {
         classname: "Meditation",
         trainer: "Maria Mich",
         date: "Fri: 1:00 pm",
+        img:"../images/classes/meditation.jpg",
     },
     {
         classname: "Boxing",
         trainer: "John Flex",
         date: "Tue: 4:00 pm",
+        img:"../images/gallery/img13.jpg",
     },
     {
         classname: "Karate",
         trainer: "David Rich",
         date: "Sat: 9:00 am",
+        img:"../images/gallery/img13.jpg",
     },
     {
         classname: "Power Lifting",
         trainer: "Larry Wheels",
         date: "Mon: 8:00 pm",
+        img:"../images/gallery/img04.jpg",
     },
     {
         classname: "Workout",
         trainer: "Shawn Ray",
         date: "Tue: 10:00 am",
+        img:"../images/gallery/img11.jpg",
     },
     {
         classname: "Crossfit",
         trainer: "Jenifer Alex",
         date: "Wen: 9:00 pm",
+        img:"../images/gallery/img15.jpg",
     },
     {
         classname: "Running",
         trainer: "Zinia Zessy",
         date: "Thu: 12:00 am",
+        img:"../images/classes/running.jpg",
     },
     {
         classname: "Fitness",
         trainer: "Mia Paul",
         date: "Fri: 12:00 pm",
+        img:"../images/gallery/img08.jpg",
     },
     {
         classname: "Body Building",
         trainer: "Paul Sneed",
         date: "Mon: 8:00 pm",
+        img:"../images/gallery/img10.jpg",
     },
     {
         classname: "Mma",
         trainer: "Becky Lynch",
         date: "Fri: 6:00 am",
+        img:"../images/gallery/img13.jpg",
     },
     {
         classname: "Yoga",
         trainer: "Marta Mich",
         date: "Wen: 8:00 am",
+        img:"../images/gallery/img05.jpg",
     },
 ]
 

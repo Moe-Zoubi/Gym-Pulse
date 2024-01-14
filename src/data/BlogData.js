aa7f30f6389ff30a8322701f3e0073e44e31d26f
unknown <m.zoubi2@hotmail.com>
2024-01-14 05:11:42 +0300

    added blog page

---
+++ src/data/BlogData.js
@@ -0,0 +1,40 @@
+import blog1 from "../images/blogs/blog1.jpg";
+import blog2 from "../images/gallery/img15.jpg";
+import blog3 from "../images/gallery/img09.jpg";
+import blog4 from "../images/gallery/img05.jpg";
+import blog5 from "../images/gallery/img13.jpg";
+import blog6 from "../images/gallery/img14.jpg";
+import blog7 from "../images/gallery/img10.jpg";
+
+const blogsData = [
+        {
+            title:"Yoga For Everyone in 2024", 
+            img:blog1,
+        },
+        {
+            title: "Getting Back Into CrossFit After Vacation",
+            img: blog2
+        },
+        {
+            title:"Meet Fitness Ambassador Grace",
+            img:blog3,
+        },
+        {
+            title:"The Best are European Materls Direct",
+            img:blog4,
+        },
+        {
+            title: "Give your Fitness a Boost with our Gym",
+            img:blog5,
+        },
+        {
+            title: "How to Get Fit Your Kids Moving Throughout The Summer",
+            img:blog6,
+        },
+        {
+            title:"Give your fitness a boost with our new gym challenge",
+            img:blog7,
+        },
+    ]
+
+export default blogsData;
--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -1,14 +1,87 @@
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/footer";
+import "./style.css";
+import blogsData from "../../data/BlogData";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import { faAngleRight, faLayerGroup, faLongArrowRight, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 
 const Blog = () => {
+
 
     return(
         <div className="blog-section">
             <Navbar />
             <div className="page-header">
                 <h2>Blog</h2>
+            </div>
+            <div className="blog-container">
+                <div className="blogs">
+                    {blogsData.map(item => (
+                        <div className="blog-comment">
+                            <img src={item.img} alt="person" />
+                            <p><FontAwesomeIcon icon={faLayerGroup} style={{color: "FE0000"}}/> By <span className="bold">Admin</span> | March 11, 2023 | Yoga</p>
+                            <h2>{item.title}</h2>
+                            <p>
+                                Authoritatively disseminate multimedia based total linkage through market-driven methodolContinually
+                                transform integrated results vis-a-vis multidisciplinary manufacture Appropriately foster fullresearched
+                                innovation rather than backend supply. when an unknown printer took a galley.
+                            </p>
+                            <button>PURCHASE NOW <FontAwesomeIcon icon={faLongArrowRight} /></button>
+                        </div>
+                    ))}
+                </div>
+                <div className="side-blog">
+                    <form className="search-bar">
+                        <input type="text" placeholder="Search..." />
+                        <button><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
+                    </form>
+                    <div className="blog-categories">
+                        <h3>Categories</h3>
+                        <div className="class-line"></div>
+                        <ul>
+                            <li>
+                                <p><FontAwesomeIcon icon={faAngleRight} /> Body Building</p>
+                                <p>(4)</p>
+                            </li>
+                            <li>
+                                <p><FontAwesomeIcon icon={faAngleRight} /> Boxing</p>
+                                <p>(4)</p>
+                            </li>
+                            <li>
+                                <p><FontAwesomeIcon icon={faAngleRight} /> Crossfit</p>
+                                <p>(4)</p>
+                            </li>
+                            <li>
+                                <p><FontAwesomeIcon icon={faAngleRight} /> Fitness</p>
+                                <p>(4)</p>
+                            </li>
+                            <li>
+                                <p><FontAwesomeIcon icon={faAngleRight} /> Meditation</p>
+                                <p>(4)</p>
+                            </li>
+                            <li>
+                                <p><FontAwesomeIcon icon={faAngleRight} /> Yoga</p>
+                                <p>(4)</p>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="latest-blogs">
+                        <h3>Recent Posts</h3>
+                        <div className="class-line"></div>
+                        {
+                            blogsData.map(item =>(
+                                <div className="latest-blog">
+                                    <img src={item.img} alt="person" />
+                                    <div className="latest-text">
+                                        <p>March 22, 2023</p>
+                                        <p>{item.title}</p>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
             </div>
             <Footer />
         </div>
